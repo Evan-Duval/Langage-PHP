9b7285969c2350f28365fff6fec26c7c6c7a97ae
Evan-Duval <duvalevan368@gmail.com>
2024-12-16 10:52:50 +0100

    add: fichier link

--- justeprix.php
+++ justeprix.php
@@ -1,31 +1,38 @@
 <?php
 
-$randomChiffre = random_int(0, 100);
-$res;
-$tentatives = 8;
+$fichiertentatives = "tentatives.txt";
+$fichier = fopen($fichiertentatives,"a");
 
-do
-{
-    echo "Donne moi un chiffre entre 0 et 100 : \n";
-    $res = readline();
+if ($fichier) {
+    $randomChiffre = random_int(0, 100);
+    $res;
+    $tentatives = 8;
 
-    if ($res > $randomChiffre) {
-        echo "C'est plus petit.\n";
-        $tentatives--;
-    } elseif ($res < $randomChiffre) {
-        echo "C'est plus grand.\n";
-        $tentatives--;
-    } elseif ($res == $randomChiffre) {
-        echo "Bravo tu as gagné !\n";
-        echo "Il te restait " . $tentatives . " tentatives\n";
-        return;
-    }
-    if ($tentatives == 0) {
-        echo "Tu as perdu, le chiffre était ". $randomChiffre. "\n";
-    } else {
-        echo "Il te reste " . $tentatives . " tentatives\n";
-    }
-    
+    do
+    {
+        echo "Donne moi un chiffre entre 0 et 100 : \n";
+        $res = readline();
 
-} 
-while ($res != $randomChiffre && $tentatives > 0);
+        if ($res > $randomChiffre) {
+            echo "C'est plus petit.\n";
+            $tentatives--;
+        } elseif ($res < $randomChiffre) {
+            echo "C'est plus grand.\n";
+            $tentatives--;
+        } elseif ($res == $randomChiffre) {
+            echo "Bravo tu as gagné !\n";
+            echo "Il te restait " . $tentatives . " tentatives\n";
+            return;
+        }
+        if ($tentatives == 0) {
+            echo "Tu as perdu, le chiffre était ". $randomChiffre. "\n";
+        } else {
+            echo "Il te reste " . $tentatives . " tentatives\n";
+        }
+        
+        fwrite($fichier, $res);
+
+    } 
+    while ($res != $randomChiffre && $tentatives > 0);
+}
+fclose($fichier);
