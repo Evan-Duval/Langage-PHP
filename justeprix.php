5b06dca45ac20059385a42a728d8aa0cbd396d1c
Evan-Duval <duvalevan368@gmail.com>
2024-12-16 11:02:51 +0100

    add: logs

--- justeprix.php
+++ justeprix.php
@@ -13,6 +13,8 @@ if ($fichier) {
         echo "Donne moi un chiffre entre 0 et 100 : \n";
         $res = readline();
 
+        fwrite($fichier, "Le nombre recherché est " . $randomChiffre . ". Le joueur à testé le nombre " . $res . ". Il lui reste " . $tentatives . " tentatives .\n\n");
+
         if ($res > $randomChiffre) {
             echo "C'est plus petit.\n";
             $tentatives--;
@@ -22,6 +24,7 @@ if ($fichier) {
         } elseif ($res == $randomChiffre) {
             echo "Bravo tu as gagné !\n";
             echo "Il te restait " . $tentatives . " tentatives\n";
+            fwrite($fichier, "Le jouer à trouvé le nombre en " . 8 - $tentatives . " tentatives. \n\n");
             return;
         }
         if ($tentatives == 0) {
@@ -30,9 +33,9 @@ if ($fichier) {
             echo "Il te reste " . $tentatives . " tentatives\n";
         }
         
-        fwrite($fichier, $res);
-
     } 
     while ($res != $randomChiffre && $tentatives > 0);
+} else {
+    echo "Erreur lors de l'ouverture du fichier";
 }
 fclose($fichier);
