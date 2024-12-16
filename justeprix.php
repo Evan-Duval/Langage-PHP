65dfc7d58a0b166cd1f6172370853547ceecfbce
Evan-Duval <duvalevan368@gmail.com>
2024-12-16 10:48:11 +0100

    add: justeprix

---
+++ justeprix.php
@@ -0,0 +1,31 @@
+<?php
+
+$randomChiffre = random_int(0, 100);
+$res;
+$tentatives = 8;
+
+do
+{
+    echo "Donne moi un chiffre entre 0 et 100 : \n";
+    $res = readline();
+
+    if ($res > $randomChiffre) {
+        echo "C'est plus petit.\n";
+        $tentatives--;
+    } elseif ($res < $randomChiffre) {
+        echo "C'est plus grand.\n";
+        $tentatives--;
+    } elseif ($res == $randomChiffre) {
+        echo "Bravo tu as gagné !\n";
+        echo "Il te restait " . $tentatives . " tentatives\n";
+        return;
+    }
+    if ($tentatives == 0) {
+        echo "Tu as perdu, le chiffre était ". $randomChiffre. "\n";
+    } else {
+        echo "Il te reste " . $tentatives . " tentatives\n";
+    }
+    
+
+} 
+while ($res != $randomChiffre && $tentatives > 0);
